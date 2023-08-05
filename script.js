2cf108378c192a1d568feb3039c32a51807c208c
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-08-05 05:10:51 +0200

    Se añade pequeño js para desplegar el menu

---
+++ script.js
@@ -0,0 +1,9 @@
+// Declaracion de variables
+const menu = document.querySelector(".header__menu");
+const nav = document.querySelector(".header__nav");
+
+// Evento para expandir o contraer el nav
+
+function desplegarMenu(){
+    nav.classList.toggle("hidden");
+};
