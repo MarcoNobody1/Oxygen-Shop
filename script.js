505af19d679161cc9598c727dad5bd4482a9eb4e
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-08-06 04:38:07 +0200

    Se añade función para cambiar svg con el ancho de pantalla

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 // Declaracion de variables
 const menu = document.getElementById("menu");
+let menuNow = "/Proyecto1/resources/svg/menu.svg";
+let menuThen = "/Proyecto1/resources/svg/x-menu.svg";
 const nav = document.getElementById("nav");
+const backimg = document.getElementById("backimg1")
 
-// Evento para expandir o contraer el nav
+// Evento para expandir o contraer el nav y cambiar la img
 
 function desplegarMenu(){
     nav.classList.toggle("hidden");
+    if (menu.src.includes(menuNow)){
+        menu.src = "/Proyecto1/resources/svg/x-menu.svg";
+    } else if (menu.src.includes(menuThen)){
+        menu.src = "/Proyecto1/resources/svg/menu.svg";
+    }
 };
+
+//Evento para cambiar la img de fondo backimg1
+
+window.addEventListener("resize", cambiarImagen)
+
+function cambiarImagen(){
+    if (window.innerWidth > 999) {
+        backimg.src = "/Proyecto1/resources/svg/Rectangle-web.svg";
+    } else {
+        backimg.src = "/Proyecto1/resources/svg/svg-c1.svg";
+    }
+}
