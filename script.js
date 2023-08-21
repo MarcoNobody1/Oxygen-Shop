580ee3e8310c39163743c459e44f15cbcc16faf4
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-08-21 02:07:58 +0200

    Se añade botón de 'Return to the top' con sus correspondientes estilos

--- script.js
+++ script.js
@@ -27,20 +27,38 @@ window.addEventListener("scroll", function barraProgreso() {
   const form = this.document.getElementById("check");
   const pt2pos = pt2.getBoundingClientRect().top;
   const pt3pos = pt3.getBoundingClientRect().top;
-  const formpos = form.getBoundingClientRect().top
+  const formpos = form.getBoundingClientRect().top;
 
   let actualWidth = (scrollY / (docHeigth - winHeigth)) * 100;
 
   bar.style.width = actualWidth + "%";
 
-  console.log(actualWidth);
-  if (pt2pos>= winHeigth){
+  if (pt2pos >= winHeigth) {
     bar.style.backgroundColor = "#08a6e4";
-  } else if (pt3pos >= winHeigth){
+  } else if (pt3pos >= winHeigth) {
     bar.style.backgroundColor = "#55DFB4";
-  } else if (formpos >= winHeigth){
+  } else if (formpos >= winHeigth) {
     bar.style.backgroundColor = "#FB3B64";
   } else {
     bar.style.backgroundColor = "#08a6e4";
   }
 });
+
+//Creación y evento del botón "Return to the top"
+
+const pt3 = document.getElementById("pt3");
+const button = document.createElement("button");
+const header = document.getElementById("header");
+button.classList.add("return");
+button.innerText = "Return to the top!";
+
+button.addEventListener("click", function () {
+  setTimeout(() => {
+    window.scrollTo({
+      top: header.offsetTop,
+      behavior: "smooth",
+    });
+  }, 200);
+});
+
+pt3.appendChild(button);
